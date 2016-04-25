282e20562a737b0eca51fb754608ca32a1fab242
Ejiroghene <ejirogheneogodo@gmail.com>
2016-04-25 09:39:07 +0100

    Getting And Cleaning Data Project

--- run_analysis.r
+++ run_analysis.r
@@ -1,71 +1,144 @@
 
-##Set working directory
-
-setwd("C:/Users/Ejiroghene/Desktop/DataScience/COURSES/Getting And Cleaning Data/UCI HAR Dataset")## set the working directory 
+##(Q1). Merge the training and the test sets to create one data set.
 
 
-##Read data sets into R
+## Set the working directory 
 
-xtest <- read.table("X_test.txt", header = FALSE)
-xtrain <- read.table("X_train.txt", header = FALSE)
+setwd("C:/Users/Ejiroghene/Desktop/DataScience/COURSES/Getting And Cleaning Data/UCI HAR Dataset")
 
 
-## load dplyr package
+
+##Read the test data set, subject data, and Activity label data into R
+
+dataTest <- read.table("X_test.txt", header = FALSE)
+activityTest <- read.table("y_test.txt", header = FALSE)
+subjectTest <- read.table("subject_test.txt", header = FALSE)
+
+
+##Read the train data set, subject data, and Activity label data into R
+
+dataTrain <- read.table("X_train.txt", header = FALSE)
+activityTrain <- read.table("y_train.txt", header = FALSE)
+subjectTrain <- read.table("subject_train.txt", header = FALSE)
+
+##Load dplyr package in order to manipulate the data
 
 library(dplyr)
 
+TestDf <- tbl_df(dataTest)
+TrainDf <- tbl_df(dataTrain)
 
-##load data into a local data frame tbl_df since I wish to use dpyr package
+activitytestDf <- tbl_df(activityTest)
+subjectTestDf <- tbl_df(subjectTest)
 
-xtestDf <- tbl_df(xtest) 
-xtrainDf <- tbl_df(xtrain)
+activityTrainDf <- tbl_df(activityTrain)
+subjectTrainDf <- tbl_df(subjectTrain)
 
-##Remove the original data frame  from workspace to avoid confusion
-rm("xtest")
-rm("xtrain")
-
-##Merge the test and train sets
-merged_Test_Train <- merge(xtestDf, xtrainDf, all = TRUE)
-
-##Extract measurements on the mean and standard deviation.
-##The column numbers correspond to the mean/standard deviation variables in the "features" file of both data sets   
-meanStd_TestTrain <- select(merged_Test_Train, V1:V6, V41:V46, V81:V86, V121:V126,
-                            V161:V166, V201:V202, V214:V215, V227:V228, V240:V241,
-                            V253:V254, V266:V271, V345:V350, V424:V429, V503:V504,
-                            V516:V517, V529:V530, V542:V543)
-
-
-
-
-
-##Appropriately label the data set with descriptive variable names.
 
 library(plyr)
 
-labeled_TestTrain <- rename(meanStd_TestTrain, c("V1"="AVG_XtBodyAcc", "V2"="AVG_YtBodyAcc", "V3"="AVG_ZtBodyAcc", "V4"="STD_XtBodyAcc", "V5"="STD_YtBodyAcc", "V6"="STD_ZtBodyAcc", 
-                                                 "V41"="AVG_XtGravityAcc", "V42"="AVG_YtGravityAcc", "V43"="AVG_ZtGravityAcc", "V44"="STD_XtGravityAcc", "V45"="STD_YtGravityAcc", "V46"="STD_ZtGravityAcc",
-                                                 "V81"="AVG_XtBodyAccJerk", "V82"="AVG_YtBodyAccJerk", "V83"="AVG_ZtBodyAccJerk", "V84"="STD_XtBodyAccJerk", "V85"="STD_YtBodyAccJerk", "V86"="STD_ZtBodyAccJerk",
-                                                 "V121"="AVG_XtGyroAcc", "V122"="AVG_YtGyroAcc", "V123"="AVG_ZtGyroAcc", "V124"="STD_XtGyroAcc", "V125"="STD_YtGyroAcc", "V126"="STD_ZtGyroAcc", 
-                                                 "V161"="AVG_XtGyroJerkAcc", "V162"="AVG_YtGyroJerkAcc", "V163"="AVG_ZtGyroJerkAcc", "V164"="STD_XtGyroJerkAcc", "V165"="STD_YtGyroJerkAcc", "V166"="STD_ZtGyroJerkAcc", 
-                                                 "V201"="AVG_tBodyAccMag", "V202"="STD_tBodyMag",
-                                                 "V214"="AVG_tGravityAccMag", "V215"="STD_tGravityAccMag", 
-                                                 "V227"="AVG_tBodyAccJerkMag", "V228"="STD_tBodyAccJerkMag", 
-                                                 "V240"="AVG_tBodyGyroMag", "V241"="STD_tBodyGyroMag", 
-                                                 "V253"="AVG_tBodyGyroJerkMag", "V254"="STD_tBodyGyroJerkMag", 
-                                                 "V266"="AVG_XfBodyAcc", "V267"="AVG_YfBodyAcc", "V268"="AVG_ZfBodyAcc", "V269"="STD_XfBodyAcc", "V270"="STD_YfBodyAcc", "V271"="STD_ZfBodyAcc", 
-                                                 "V345"="AVG_XfBodyAccJerk", "V346"="AVG_YfBodyAccJerk", "V347"="AVG_ZfBodyAccJerk", "V348"="STD_XfBodyAccJerk", "V349"="STD_YfBodyAccJerk", "V350"="STD_ZfBodyAccJerk", 
-                                                 "V424"="AVG_XfBodyGyro", "V425"="AVG_YfBodyGyro", "V426"="AVG_ZfBodyGyro", "V427"="STD_XfBodyGyro", "V428"="STD_YfBodyGyro", "V429"="STD_ZfBodyGyro", 
-                                                 "V503"="AVG_fBodyAccMag", "V504"="STD_fBodyMag",
-                                                 "V516"="AVG_fBodyAccJerkMag", "V517"="STD_fBodyAccJerkMag",
-                                                 "V529"="AVG_fBodyGyroMag", "V530"="STD_fBodyGyroMag",
-                                                 "V542"="AVG_fBodyGyroJerkMag", "V543"="STD_fBodyGyroJerkMag"
-)
-)
+##Rename the variables(columns) in the test and train activity label and subject data frames since the columns don't have names. 
+
+activityTestDf <- rename(activitytestDf, c("V1" = "Activity"))
+subjectTestDf<- rename(subjectTest, c("V1" = "Subject"))
+
+activityTrainDf <- rename(activityTrainDf, c("V1" = "Activity"))
+subjectTrainDf<- rename(subjectTrainDf, c("V1" = "Subject"))
+
+test <- cbind(activityTestDf, subjectTestDf, TestDf)
+train <- cbind(activityTrainDf, subjectTrainDf,TrainDf)
 
 
-## Select only the average variables 
-avgVariables_TestTrain <-select(labeled_TestTrain, contains("AVG"))
+##Merge the test and train data frame 
+
+testTrain <- merge(test, train, all = TRUE)
 
 
-library(tidyr)
-tidyDataActivityAverage <- gather(avg_TestTrain, Activity, Average )
+
+
+
+##After the merge it was noticed that "Activity" and "Subject variables were 
+##placed in the 1st & 2nd indexes respectively, this 
+##may affect the  manipulaaton of the data set since these varibles were not included in the original 
+##features.txt file that has indices of 1:561. 
+
+##Reorder the data frame so that "Activity" and "Subject" variables will be the last two variables
+
+testTrain <- testTrain[, c(3:563,1,2)]
+
+
+##Q2). Extract only the measurements on the mean and standard deviation for each measurement.
+
+##Read the features.txt dataframe into R
+
+variableNames <- read.table("features.txt", header = FALSE)
+
+
+
+# The columns are filterd out from  "variableNames" by getting a list of 
+##indices indicating the positions occupied by column names with mean or standard deviation.
+
+
+meanStdColIndices <- grep("mean|std", variableNames$V2, value = FALSE) 
+
+
+##Get a subset of the merged data that has  columns with mean/std
+##(ie the extracted data with only mean and standard deviation variables)
+
+testTrain_Mean_Std <- testTrain[, c(1, 2, 3,  4, 5, 6,  41,  42, 43, 44, 45, 46, 81, 82, 83, 84, 85, 86,
+                                    121, 122, 123, 124, 125, 126, 161, 162,163, 164, 165, 166, 201, 202, 214, 215, 227, 228,
+                                    240, 241, 253, 254, 266, 267, 268, 269, 270, 271, 294, 295, 296, 345, 346, 347,348, 349,
+                                    350, 373, 374, 375, 424, 425, 426, 427, 428, 429, 452, 453, 454, 503, 504, 513, 516, 517,
+                                    526, 529, 530, 539, 542, 543,552)]
+
+
+
+
+
+##Q3). Use descriptive activity names to name the activities in the data set
+
+##Assign activity to each of the corresponding figure as given in activity_label.txt file.
+
+testTrain$Activity[testTrain$Activity == 1] <- 'WALKING'
+testTrain$Activity[testTrain$Activity == 2] <- 'WALKING_UPSTAIRS'
+testTrain$Activity[testTrain$Activity == 3] <- 'WALKING_DOWNSTAIRS'
+testTrain$Activity[testTrain$Activity == 4] <- 'SITTING'
+testTrain$Activity[testTrain$Activity == 5] <- 'STANDING'
+testTrain$Activity[testTrain$Activity == 6] <- 'LAYING'
+
+
+
+
+##Q4). Appropriately label the data set with descriptive variable names. 
+
+mean_Std_Names <- grep("mean|std", variableNames$V2, value = TRUE) 
+
+
+
+
+
+testTrainDf <- tbl_df(testTrain)
+
+
+varNames <- select(variableNames,  V2)
+
+
+colnames(testTrainDf) <- varNames[1:561,]
+colnames(testTrainDf)[562] <- "Activity"
+colnames(testTrainDf)[563] <- "Subject"
+
+
+
+##Remove any occurence of duplicate variable name since this a common occurrence with joined/merged data set
+
+colnames(testTrainDf) = make.names(colnames(testTrainDf), unique=TRUE)
+
+
+##From the data set in step 4, create a second, independent tidy data set with 
+##the average of each variable for each activity and each subject.
+
+
+tidyData<- (testTrainDf%>%
+              group_by(Subject,Activity) %>%
+              summarise_each(funs( mean)))
+
